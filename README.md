6c7f368903d0c45527303993709e38fd57b65084
stamfar <69307900+mainlyno@users.noreply.github.com>
2021-11-16 16:42:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# sanity-plugin-plausible-iframe
+
+Simple plugin to show Plausible analytics as an iframe inside of the Sanity Dashboard.
+
+## Installation
+
+```
+sanity install plausible-iframe
+```
+
+## Configuration
+
+The plugin can be configured through `<your-studio-folder>/config/plausible-iframe.json`:
+
+```json
+{
+  "add-config": "here"
+}
+```
+
+## License
+
+MIT © Ole Svennevig / Mainly
+See LICENSE
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {DashboardWidget} from "@sanity/dashboard";
+import config from 'config:plausible-iframe'
+
+// Values from Plausible embed url. 
+// Only 'site' needs to be changed in sanity config.
+const root = 'https://plausible.io/share'
+const site = `${config.site}` //'YOUR SITE HERE'
+const embed = 'true'
+const theme = 'light'
+const background = 'transparent'
+
+const PlausibleIframe = () => {
+  return (
+  <DashboardWidget header="Plausible statistics">
+    <>
+      <iframe plausible-embed
+      src={`${root}/${site}&embed=${embed}&theme=${theme}&background=${background}`}
+      scrolling="no" frameborder="0" loading="lazy" 
+      style={{width: '1px', minWidth: '100%', height: '1600px'}}>
+      </iframe>
+      <script async src="https://plausible.io/js/embed.host.js"></script>
+    </>
+  </DashboardWidget>
+  )
+}
+
+export default {
+  name: "plausible-iframe",
+  component: PlausibleIframe,
+};
